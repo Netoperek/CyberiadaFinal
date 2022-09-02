59a4fc031425c69a6af2e624d7abb0dd65e6b160
Netoperek <q.krzysztof-chylinski.p@wp.pl>
2022-09-02 21:55:56 +0200

    added Player Controller

--- Assets/C#/PlayerController.cs
+++ Assets/C#/PlayerController.cs
@@ -2,17 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [field: SerializeField] Vector3 leftPos;
+    [field: SerializeField] Vector3 centerPos;
+    [field: SerializeField] Vector3 rightPos;
+    [field: SerializeField] float speedMove { get; set; }
+    // [SerializeReference] InputActionPlayer inputs;
+    [SerializeReference] Rigidbody rb;
+    Vector2 startPos;
+    private enum Stat { left, center, right }
+    private Stat current;
+    private void Update()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startPos = touch.position;
+                    break;
+                case TouchPhase.Moved:
+
+                    break;
+                case TouchPhase.Stationary:
+                    break;
+                case TouchPhase.Ended:
+                    if (startPos.x < touch.position.x)
+                    {
+                        Move(Stat.right);
+                    }
+                    else
+                    {
+                        Move(Stat.left);
+                    }
+                    break;
+                case TouchPhase.Canceled:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+    private void Move(Stat stat)
     {
-        
+        switch (current)
+        {
+            case Stat.left:
+                rb.transform.position = centerPos;
+                current = Stat.center;
+                break;
+            case Stat.center:
+                if (stat == Stat.right)
+                {
+                    rb.transform.position = rightPos;
+                    current = Stat.right;
+                }
+                else if (stat == Stat.left)
+                {
+                    rb.transform.position = leftPos;
+                    current = Stat.left;
+                }
+
+                break;
+            case Stat.right:
+                rb.transform.position = centerPos;
+                current = Stat.center;
+                break;
+            default:
+                break;
+        }
+
     }
 }
